ccd9700fdd33054937d777c909cca6353b1c5a1a
Karnphong Saratontiphong <inseekan@gmail.com>
2025-10-29 19:50:08 +0700

    price add

--- main.js
+++ main.js
@@ -66,8 +66,8 @@ function displayRandomProducts(num = 3) {
         productDiv.innerHTML = `
             <h3>${product.name}</h3>
             ${sliderHTML}
-            <h5>Price: ${product.description}</h5>
-            <h6>link: ${product.link}</h6>
+            <h5>Price: ${product.price}</h5>
+            <a>link: ${product.link}</a>
             <p>${product.description}</p>
         `
 
@@ -118,3 +118,44 @@ changeSlide();
 
 
 setInterval(changeSlide, 5000);
+
+
+
+// Price Slider Functionality
+const rangeMin = document.getElementById('range-min');
+const rangeMax = document.getElementById('range-max');
+const minValue = document.getElementById('min-value');
+const maxValue = document.getElementById('max-value');
+const sliderTrack = document.getElementById('slider-track');
+const minPriceInput = document.getElementById('min-price');
+const maxPriceInput = document.getElementById('max-price');
+
+function updateSlider() {
+    let min = parseInt(rangeMin.value);
+    let max = parseInt(rangeMax.value);
+    
+    if (max - min < 5) {
+        if (this.id === 'range-min') {
+            rangeMin.value = max - 5;
+            min = max - 5;
+        } else {
+            rangeMax.value = min + 5;
+            max = min + 5;
+        }
+    }
+    
+    minValue.textContent = min;
+    maxValue.textContent = max;
+    minPriceInput.value = min;
+    maxPriceInput.value = max;
+    
+    const percent1 = (min / rangeMin.max) * 100;
+    const percent2 = (max / rangeMax.max) * 100;
+    sliderTrack.style.left = percent1 + '%';
+    sliderTrack.style.width = (percent2 - percent1) + '%';
+}
+
+rangeMin.addEventListener('input', updateSlider);
+rangeMax.addEventListener('input', updateSlider);
+
+updateSlider();
