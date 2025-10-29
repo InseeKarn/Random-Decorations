0ae18cfbc47ee755d45e2542afd975c9b1019f6f
Karnphong Saratontiphong <inseekan@gmail.com>
2025-10-29 17:02:20 +0700

    321

---
+++ main.js
@@ -0,0 +1,53 @@
+const btn_random = document.getElementById('randomize');
+
+const product = [
+    { name: "โคมไฟวินเทจ", price: "$25", imae: ["lamp1.jpg", "lamp2.jpg", "lamp3.jpg"], description: "โคมไฟตกแต่งบ้านสไตล์วินเทจ", link: "#" },
+    { name: "โต๊ะข้างเตียง", price: "$45", image: ["table.jpg", "table.jpg", "table.jpg"], description: "โต๊ะข้างเตียงไม้สวย", link: "#", link: "#" },
+    { name: "เก้าอี้โมเดิร์น", price: "$60", image: ["chair.jpg", "chair.jpg", "chair.jpg"], description: "เก้าอี้โมเดิร์นสำหรับห้องนั่งเล่น", link: "#" },
+    { name: "ชั้นวางของ", price: "$35", image: ["shelf.jpg", "shelf.jpg", "shelf.jpg"], description: "ชั้นวางของไม้สีอ่อน", link: "#" }
+];
+
+function displayRandomProducts(num = 3) {
+    const container = document.getElementById('product-container');
+
+    container.innerHTML = "";
+
+    const shuffled = product.sort(() => 0.5 - Math.random());
+
+    const selected = shuffled.slice(0, num);
+
+    // slider
+    let sliderHTML = `
+        <div class="slider" id="slider-${index}">
+            <button class="prev">&#10094;</button>
+            <img src="${product.images[0]}" alt="${product.name}">
+            <button class="next">&#10095;</button>
+        </div>
+    `;
+
+    selected.forEach(product => {
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
+        productDiv.innerHTML = `
+            <h3>${product.name}</h3>
+            <img src="${product.image}" alt="${product.name}">
+            <h5>Price: ${product.description}</h5>
+            <h6>link: ${product.link}</h6>
+            <p>${product.description}</p>
+        `
+
+        container.appendChild(productDiv);
+
+        let currentIndex = 0;
+        const slider = productDiv.quearySelector(`#slider-${index} img`)
+
+    })
+}
+
+displayRandomProducts(3);
+
+btn_random.addEventListener('click', () => {
+    displayRandomProducts(3);
+})
+
